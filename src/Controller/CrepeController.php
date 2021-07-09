f139fb2efb5322ea8ff8a3ea0673d20b18e05970
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-09 10:33:58 +0200

    btn show

--- src/Controller/CrepeController.php
+++ src/Controller/CrepeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CrepeController extends AbstractController
 {
     /**
-     * @Route("/crepe", name="crepe")
+     * @Route("/crepe", name="crepeIndex")
      */
     public function index(): Response
     {
@@ -63,4 +63,60 @@ class CrepeController extends AbstractController
             'tabCrepes' => $crepes
         ]);
     }
+
+    /**
+     * @Route("/crepe/{crepeId}", name="crepeShow")
+     */
+    public function show(int $crepeId): Response
+    {
+
+
+        $crepe1 = [
+            "id" => 1,
+            "name" => "mariette",
+            "type" => "sucre",
+            "price" => 8,
+            "description" => "La spécialité de Bourg-en-Bourg",
+            "createdAt" => "09/07/2020"
+        ];
+
+        $crepe2 = [
+            "id" => 2,
+            "name" => "jacki",
+            "type" => "sale",
+            "price" => 4,
+            "description" => "La spécialité de Lyonne",
+            "createdAt" => "08/12/2001"
+        ];
+
+        $crepe3 = [
+            "id" => 3,
+            "name" => "franx",
+            "type" => "sale",
+            "price" => 5,
+            "description" => "La spécialité de Cilus",
+            "createdAt" => "12/03/2017"
+        ];
+
+        $crepe4 = [
+            "id" => 4,
+            "name" => "france",
+            "type" => "sucre",
+            "price" => 6,
+            "description" => "La spécialité de fransium",
+            "createdAt" => "12/09/2010"
+        ];
+
+        $crepes = [
+            $crepe1['id'] => $crepe1,
+            $crepe2['id'] => $crepe2,
+            $crepe3['id'] => $crepe3,
+            $crepe4['id'] =>$crepe4,
+        ];
+
+        return $this->render('crepe/show.html.twig', [
+            'uneCrepe' => $crepes[$crepeId],
+        ]);
+    }
+
 }
