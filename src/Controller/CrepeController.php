8d4e6bbd3eb87594266786091c29baed66132090
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-09 15:16:48 +0200

    recup data base, first entity, first migration

--- src/Controller/CrepeController.php
+++ src/Controller/CrepeController.php
@@ -2,61 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\CrepeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Crepe;
 
 class CrepeController extends AbstractController
 {
     /**
      * @Route("/crepe", name="crepeIndex")
      */
-    public function index(): Response
+    public function index(CrepeRepository $repo): Response
     {
-
-
-        $crepe1 = [
-            "id" => 1,
-            "name" => "mariette",
-            "type" => "sucre",
-            "price" => 8,
-            "description" => "La spécialité de Bourg-en-Bourg",
-            "createdAt" => "09/07/2020"
-        ];
-
-        $crepe2 = [
-            "id" => 2,
-            "name" => "jacki",
-            "type" => "sale",
-            "price" => 4,
-            "description" => "La spécialité de Lyonne",
-            "createdAt" => "08/12/2001"
-        ];
-
-        $crepe3 = [
-            "id" => 3,
-            "name" => "franx",
-            "type" => "sale",
-            "price" => 5,
-            "description" => "La spécialité de Cilus",
-            "createdAt" => "12/03/2017"
-        ];
-
-        $crepe4 = [
-            "id" => 4,
-            "name" => "france",
-            "type" => "sucre",
-            "price" => 6,
-            "description" => "La spécialité de fransium",
-            "createdAt" => "12/09/2010"
-        ];
-
-        $crepes = [
-            $crepe1,
-            $crepe2,
-            $crepe3,
-            $crepe4,
-        ];
+        $crepes = $repo->findAll();
 
         return $this->render('crepe/index.html.twig', [
             'controller_name' => 'CrepeController',
@@ -65,57 +24,13 @@ class CrepeController extends AbstractController
     }
 
     /**
-     * @Route("/crepe/{crepeName}", name="crepeShow")
+     * @Route("/crepe/{name}", name="crepeShow")
      */
-    public function show(string $crepeName): Response
+    public function show(Crepe $crepe, $name): Response
     {
 
-
-        $crepe1 = [
-            "id" => 1,
-            "name" => "mariette",
-            "type" => "sucre",
-            "price" => 8,
-            "description" => "La spécialité de Bourg-en-Bourg",
-            "createdAt" => "09/07/2020"
-        ];
-
-        $crepe2 = [
-            "id" => 2,
-            "name" => "jacki",
-            "type" => "sale",
-            "price" => 4,
-            "description" => "La spécialité de Lyonne",
-            "createdAt" => "08/12/2001"
-        ];
-
-        $crepe3 = [
-            "id" => 3,
-            "name" => "franx",
-            "type" => "sale",
-            "price" => 5,
-            "description" => "La spécialité de Cilus",
-            "createdAt" => "12/03/2017"
-        ];
-
-        $crepe4 = [
-            "id" => 4,
-            "name" => "france",
-            "type" => "sucre",
-            "price" => 6,
-            "description" => "La spécialité de fransium",
-            "createdAt" => "12/09/2010"
-        ];
-
-        $crepes = [
-            $crepe1['name'] => $crepe1,
-            $crepe2['name'] => $crepe2,
-            $crepe3['name'] => $crepe3,
-            $crepe4['name'] => $crepe4,
-        ];
-
         return $this->render('crepe/show.html.twig', [
-            'uneCrepe' => $crepes[$crepeName],
+            'uneCrepe' => $crepe
         ]);
     }
 
